33b73591c4745a2e66119019db65d8180b2e9796
teklumezgebo <mezge002@umn.edu>
2023-08-31 15:37:10 -0500

    users controller completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
+
+    def user_not_found
+        render json: { error: "User not found" }, status: :not_found 
+    end
 end
