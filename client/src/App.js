2f22255e1559a6fd7ad5fb2ec7b8b5f3f0c141ee
teklumezgebo <mezge002@umn.edu>
2023-09-04 15:51:01 -0500

    useContext state mangement implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Navbar from "./NavBar"
-import './index.css';
+import { useUserContext } from "./UserContext"
 
 function App() {
+  const { user, setUser } = useUserContext()
+
   return (
     <div>
       <Navbar />
-        
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { UserProvider } from './UserContext';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +9,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
