2c798e2368f3a111068abefa3fab3885fef4471c
teklumezgebo <mezge002@umn.edu>
2023-09-09 20:03:14 -0500

    careRequirements/newData objects created for form submission

--- client/src/components/Entry.js
+++ client/src/components/Entry.js
@@ -3,19 +3,21 @@ import { useParams } from "react-router-dom";
 import { useUserContext } from "../UserContext";
 import { Line } from "react-chartjs-2";
 import { Chart as Chart } from "chart.js/auto"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 
 function Entry() {
     const { id } = useParams()
     const { user } = useUserContext()
 
-    const [duration, setDuration] = useState()
-    const [intensity, setIntensity] = useState()
-    const [frequency, setFrequency] = useState()
-    const [nutrients, setNutrients] = useState()
+    const [duration, setDuration] = useState('')
+    const [intensity, setIntensity] = useState('')
+    const [frequency, setFrequency] = useState('')
+    const [nutrients, setNutrients] = useState('')
+    const [selectedDate, setSelectedDate] = useState(new Date())
+    const [measurementValue, setMeasurementValue] = useState('')
 
     const plant = user.plants.find(plant => plant.id === parseInt(id))
-
-    console.log(plant)
 
     const chartdata = {
         labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
@@ -23,6 +25,18 @@ function Entry() {
             label: "amount",
             data: [1,2,3,4,5,6,7]
         }]
+    }
+
+    const careRequirementsObj = {
+        watering_frequency: frequency,
+        light_duration: duration,
+        light_intensity: intensity,
+        nutrients: nutrients
+    }
+
+    const newDataObj = {
+        measurement_date: selectedDate,
+        measurement_value: measurementValue
     }
     
     return(
@@ -38,22 +52,24 @@ function Entry() {
                     </div>
                 </div>
                 <div className="grid-col-1 w-full h-full ml-20 bg-[#FFE8D6] rounded-lg p-5">
-                    <div className="bg-[#D6E0FF] p-5 h-80 text-black text-left text-3xl font-extrabold rounded-lg w-full">
+                    <div className="bg-[#D6E0FF] p-5 mt-5 h-80 text-black text-left text-3xl font-extrabold rounded-lg w-full">
                         <h1 className="text-5xl">Care Requirements</h1>
                         <h1 className="mt-5">Light Duration: placeholder</h1>
                         <h1 className="mt-3">Light intensity: placeholder</h1>
                         <h1 className="mt-3">Watering Frequency: placeholder</h1>
                         <h1 className="mt-3">Nutrients: placeholder</h1>
                     </div>
-                    <div className="flex bg-[#D6E0FF] h-96 p-5 mt-10 rounded-lg w-full justify-center">
-                        <div className="bg-[#FFF5D6] mr-32 w-64 rounded-lg">
-                            <form>
-                                <input className="bg-white m-10 " type="text"/>
-                                <input className="bg-white m-10 mt-5" type="text" />
-                                <input className="bg-white mt-24 ml-4 rounded-md p-3 hover:scale-105 duration-200 cursor-pointer" type="submit" value="Record" />
+                    <div className="bg-[#D6E0FF] h-96 p-5 mt-14 rounded-lg w-full">
+                        <div className="bg-[#FFF5D6] ml-4 w-64 rounded-lg">
+                            <form className="h-84 mt-1">
+                                <h1 className="ml-4 -mb-4">Height/Weight</h1>
+                                <input className="bg-white m-4 rounded-md p-2 mt-6" type="text"/>
+                                <h1 className="ml-4 mb-2">Date</h1>
+                                <DatePicker className="bg-white ml-4 rounded-md p-2" selected={selectedDate} onChange={(date) => setSelectedDate(date)}/>
+                                <input className="bg-white mt-24 ml-4 mb-3 rounded-md p-3 hover:scale-105 duration-200 cursor-pointer" type="submit" value="Record" />
                             </form>
                         </div>
-                        <Line className="bg-[#FFF5D6] rounded-lg p-4" data={chartdata} />   
+                        <Line className="bg-[#FFF5D6] rounded-lg p-5 ml-96 -mt-80" data={chartdata} />   
                     </div> 
                 </div> 
             </div>           
