0baebff8b307239d4a12976ace1f2fd005b17b53
teklumezgebo <mezge002@umn.edu>
2023-09-18 16:41:16 -0500

    guide component created

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
+import SearchCard from "./SearchCard";
 
 function Dashboard() {
     const [search, setSearch] = useState('')
-    const [results, setResults] = useState('')
+    const [results, setResults] = useState(null)
 
     function handleSearch(e) {
         e.preventDefault()
-        fetch(`https://perenual.com/api/species-care-guide-list?key=sk-96T76508ba2106a792196&q=${search}`)
+        fetch(`https://perenual.com/api/species-care-guide-list?key=&q=${search}`)
         .then(res => {
             if (res.ok) {
-                res.json().then(plants => setResults(plants))
+                res.json().then(plants => {
+                    setResults(plants)
+                    setSearch('')
+                })
             } else {
                 res.json().then(res => console.log(res))
             }
@@ -17,16 +21,14 @@ function Dashboard() {
     }
 
     return (
-        <div className="h-screen w-full grid grid-cols-3 place-items-start bg-[#4A4E69] p-6 m-9 rounded-2xl">
-            <form className="bg-[#C9ADA7] col-span-2 p-4 w-1/3 h-2/5 grid grid-cols-1 gap-2 rounded-lg shadow-lg">
-                <input className="rounded-xl text-left p-2 text-xl h-4/5" placeholder="Search Plants..." value={search} onChange={e => setSearch(e.target.value)}/>
-                <input className="bg-[#22223B] text-[#F2E9E4] rounded-xl hover:cursor-pointer hover:scale-105 duration-150 font-semibold" type="submit" value="Search"/>
+        <div className={`${results ? 'h-full' : 'h-screen'} w-full grid grid-cols-3 place-items-start gap-5 bg-[#4A4E69] p-6 m-9 rounded-2xl`}>
+            <form className="bg-[#C9ADA7] col-span-2 p-4 w-3/5 h-26 grid grid-cols-2 gap-3 rounded-lg shadow-lg" onSubmit={handleSearch}>
+                <input className="rounded-xl text-left p-2 text-xl h-20" placeholder="Search Plant Guides..." value={search} onChange={e => setSearch(e.target.value)}/>
+                <input className="bg-[#22223B] text-[#F2E9E4] h-20 rounded-xl hover:cursor-pointer hover:scale-105 duration-150 font-semibold" type="submit" value="Search"/>
             </form>
-            <div className="bg-[#F2E9E4] col-span-3 w-full h-full rounded-3xl grid grid-cols-1 p-4 place-items-start gap-2">
-                <div>Name</div>
-                <div>Name</div>
-                <div>Name</div>
-            </div>
+            {results ? <div className="bg-[#F2E9E4] col-span-3 w-full h-full rounded-3xl grid grid-cols-6 p-8 place-items-center gap-4">
+                {results.data.map(plant => <SearchCard key={plant.id} id={plant.id} commonName={plant.common_name} scientificName={plant.scientific_name} section={plant.section} />)}
+            </div> : null}
         </div>
     )
 }
---
+++ client/src/components/Guide.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Guide() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Guide
